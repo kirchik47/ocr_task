8fcd2e017bea102413289078f32ef8b41ccdfd83
kirchik47 <kirchikimp@gmail.com>
2024-09-28 15:00:16 +0200

    Initial model tests
    Tested both GOT and Qwen models. Both are performing pretty well for documents in English, however for hindi performance is pretty poor. GOT's tokenizer is based only on latin symbols. For Qwen probably we can do some prompt engineering to make it not translate Hindi text to English. There are two possible ways to implement Hindi OCR: either train GOT or Qwen on hindi documents dataset, it will require tokenizer adaptation too, or to apply some prompt engineering techniques to make Qwen LLM generate hindi text.

---
+++ main.py
@@ -0,0 +1,47 @@
+from transformers import AutoModel, AutoTokenizer
+import torch
+from byaldi.RAGModel import RAGMultiModalModel
+from byaldi.colpali import ColPaliModel
+from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
+import torch
+from PIL import Image
+import numpy as np
+
+
+# rag = ColPaliModel.from_pretrained('vidore/colpali')
+
+model = Qwen2VLForConditionalGeneration.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", torch_dtype=torch.bfloat16).eval()
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct", trust_remote_code=True)
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "image": Image.open('template.jpg'),
+            },
+            {"type": "text", "text": 'Return full text of the document as a plain text'},
+        ],
+    }
+]
+
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+img = Image.open('hindi_template.jpg')
+inputs = processor(
+    text=text,
+    images=img,
+    padding=True,
+    return_tensors="pt",
+)
+inputs = inputs.to("cpu")
+generated_ids = model.generate(**inputs, max_new_tokens=5000)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+]
+output_text = processor.batch_decode(
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+)
+print(output_text)
---
+++ main_got.py
@@ -0,0 +1,44 @@
+from transformers import AutoModel, AutoTokenizer
+import torch
+
+
+if torch.cuda.is_available():
+    device = torch.device('cuda') # If cuda is available, use it, otherwise use CPU
+else:
+    device = torch.device('cpu')
+
+tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', 
+                                          trust_remote_code=True # Allows custom code to load model from hub
+            )
+model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', 
+                                  trust_remote_code=True, 
+                                  low_cpu_mem_usage=True, 
+                                  device_map=device.type, 
+                                  use_safetensors=True, # This format is faster, more memory efficient 
+                                                        # and provides safe deserialization unlike pickle-based one
+                                  pad_token_id=tokenizer.eos_token_id # Set the pad token from tokenizer
+        )
+
+
+# input your test image
+image_file = 'template.jpg'
+
+res = model.chat(tokenizer, image_file, ocr_type='ocr')
+
+# format texts OCR:
+# res = model.chat(tokenizer, image_file, ocr_type='format')
+
+# fine-grained OCR:
+# res = model.chat(tokenizer, image_file, ocr_type='ocr', ocr_box='')
+# res = model.chat(tokenizer, image_file, ocr_type='format', ocr_box='')
+# res = model.chat(tokenizer, image_file, ocr_type='ocr', ocr_color='')
+# res = model.chat(tokenizer, image_file, ocr_type='format', ocr_color='')
+
+# multi-crop OCR:
+# res = model.chat_crop(tokenizer, image_file, ocr_type='ocr')
+# res = model.chat_crop(tokenizer, image_file, ocr_type='format')
+
+# render the formatted OCR results:
+# res = model.chat(tokenizer, image_file, ocr_type='format', render=True, save_render_file = './demo.html')
+
+print(res)
