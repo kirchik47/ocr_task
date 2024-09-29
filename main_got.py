93ba5a57df0595504f19be5650b7e259c6238e93
kirchik47 <kirchikimp@gmail.com>
2024-09-29 16:31:49 +0200

    Modifications to main file

--- main_got.py
+++ main_got.py
@@ -8,10 +8,10 @@ def extract_text(image_path):
     else:
         device = torch.device('cpu')
 
-    tokenizer = AutoTokenizer.from_pretrained('GOT-OCR2_0', 
+    tokenizer = AutoTokenizer.from_pretrained('custom_got', 
                                             trust_remote_code=True # Allows custom code to load model from hub
                 )
-    model = AutoModel.from_pretrained('GOT-OCR2_0', 
+    model = AutoModel.from_pretrained('custom_got', 
                                     trust_remote_code=True, 
                                     low_cpu_mem_usage=True, 
                                     device_map=device.type, 
@@ -24,4 +24,3 @@ def extract_text(image_path):
     # Extract text
     res = model.chat(tokenizer, image_file, ocr_type='ocr')
     return res
-extract_text('data_80k/output_images/1.png')
