10b261ea5a42710d575d4a63e44cec7435787d65
kirchik47 <kirchikimp@gmail.com>
2024-09-29 12:51:20 +0200

    Added requirements and cloned repos for fine-tuning
    Cloned GOT repo and ms-swift for fine-tuning according to instructions in README in GOT repo. Generated dataset in json format required for ms-swift.

---
+++ dataset_creation.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+import os
+
+
+dataset = pd.read_csv('data_80k/data.csv')
+labels = dataset['image_file']
+text = dataset['text']
+json_data = []
+images_path = 'data_80k/output_images/'
+for i in range(len(labels)):
+    json_data.append(
+        {
+            "query": "<image>",
+            "response": text[i],
+            "images": [os.path.join(images_path, labels[i])],
+        }
+    )
+with open('dataset.json', 'w') as f:
+    json.dump(json_data, f)
+    
